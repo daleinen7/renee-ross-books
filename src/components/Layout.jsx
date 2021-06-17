4919754d1e42ee48691fa0b00340535f07de03d8
daleinen7 <daleinen@gmail.com>
2021-06-17 13:36:24 -0500

    add background image

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { createGlobalStyle } from "styled-components"
+import { StaticImage } from 'gatsby-plugin-image';
 import "@fontsource/tangerine"
 import "@fontsource/raleway"
 import Menu from './Menu';
@@ -7,18 +8,27 @@ import Menu from './Menu';
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #202122;
+    background: url();
     color: #FCFDFE;
+    margin: 0;
 
     font-family: 'Raleway', sans-serif;
 
     h1, h2, h3, h4, h5, h6 {
       font-family: 'Tangerine', serif;
     }
+
+    .bg {
+      top: 0;
+      position: absolute;
+      z-index: -5;
+    }
   }
 `
 export default function Layout({ children }) {
   return (
     <>
+      <StaticImage className="bg" src="../images/HeaderwotextDark.jpg" />
       <GlobalStyle />
       <h1>Renee Ross Books</h1>
       <Menu/>
