377160c240c6ac14d0df65a052c96e1b19a885bb
daleinen7 <daleinen@gmail.com>
2021-06-17 15:37:13 -0500

    logo and nav styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import React from "react"
+import { Link } from 'gatsby';
 import { createGlobalStyle } from "styled-components"
 import { StaticImage } from 'gatsby-plugin-image';
 import "@fontsource/tangerine"
 import "@fontsource/raleway"
 import Menu from './Menu';
+import styled from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #202122;
-    background: url();
     color: #FCFDFE;
     margin: 0;
 
@@ -25,12 +26,35 @@ const GlobalStyle = createGlobalStyle`
     }
   }
 `
+const Logo = styled.h1`
+  font-size: 96px;
+  margin: 2rem 0 2rem 2rem;
+  text-shadow: 7px 7px 4px #202020;
+
+  a {
+    text-decoration: none;
+    color: white;
+    &:hover {
+      text-shadow: 2px 2px 40px #a81010;
+    }
+  }
+
+  [role] {
+    font-family: 'Raleway', serif;
+    font-size: 1.3rem;
+  }
+`;
+
+
 export default function Layout({ children }) {
   return (
     <>
       <StaticImage className="bg" src="../images/HeaderwotextDark.jpg" />
       <GlobalStyle />
-      <h1>Renee Ross Books</h1>
+      <Logo><Link to='/'>Renee Ross Books</Link>
+      <div role="doc-subtitle">Gothic Romance the Way You Remember...</div>
+      </Logo>
+
       <Menu/>
       {children}
     </>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,19 @@ import { Link, StaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
 
 const Navigation = styled.nav`
-  
+  ul{ 
+    list-style-type: none;
+  }
+
+  a {
+    color: #eae3e3;
+    text-decoration: none;
+    font-size: 1.2rem;
+    &:hover {
+      color: white;
+      text-shadow: 2px 2px 40px #a81010;
+    }
+  }
 `;
 
 export default function Menu() {
@@ -23,7 +35,7 @@ export default function Menu() {
       render={data => (
         <Navigation>
           <ul>
-            <li><Link to='/'>Home</Link></li>
+            {/* <li><Link to='/'>Home</Link></li> */}
             {data.allDatoCmsBook.nodes.map(book => {
               return <li><Link to={`/${book.slug}`}>{book.title}</Link></li>
             })}
