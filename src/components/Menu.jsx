0f9d76df7afc27bf686e5a8b7f479cc152d8ed36
Stephanie <Rinehart>
2021-06-20 19:10:16 -0400

    create bootstrap navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link, StaticQuery, graphql } from 'gatsby';
+import {
+    Navbar,
+    Nav,
+    NavDropdown
+} from 'react-bootstrap';
 import styled from 'styled-components';
 
-const Navigation = styled.nav`
-  ul{ 
-    list-style-type: none;
-  }
+// const Navigation = styled.nav`
+//   ul{ 
+//     list-style-type: none;
+//   }
 
-  a {
-    color: #eae3e3;
-    text-decoration: none;
-    font-size: 1.2rem;
-    &:hover {
-      color: white;
-      text-shadow: 2px 2px 40px #a81010;
-    }
-  }
-`;
+//   a {
+//     color: #eae3e3;
+//     text-decoration: none;
+//     font-size: 1.2rem;
+//     &:hover {
+//       color: white;
+//       text-shadow: 2px 2px 40px #a81010;
+//     }
+//   }
+// `;
 
 export default function Menu() {
   return(
@@ -33,14 +38,26 @@ export default function Menu() {
         }
       `}
       render={data => (
-        <Navigation>
-          <ul>
-            {/* <li><Link to='/'>Home</Link></li> */}
+        // <Navigation>
+
+        <Navbar bg="dark" variant="dark" expand="lg" className="p-4 mb-4">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <NavDropdown title="Books" id="navbarScrollingDropdown">   
             {data.allDatoCmsBook.nodes.map(book => {
-              return <li><Link to={`/${book.slug}`}>{book.title}</Link></li>
+              return <NavDropdown.Item href={`/${book.slug}`}>{book.title}</NavDropdown.Item>
             })}
-          </ul>
-        </Navigation>
+            </NavDropdown>
+            
+        </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link>For Social Links</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
       )}
     />
   )
