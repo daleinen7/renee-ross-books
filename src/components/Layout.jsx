684374adf8bd9e96e55985e8ad0bc73ba04f6213
Stephanie <Rinehart>
2021-06-27 20:52:26 -0400

    change styling, adjust layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,9 @@ import React from "react"
 import { Link } from 'gatsby';
 import { createGlobalStyle } from "styled-components"
 import { StaticImage } from 'gatsby-plugin-image';
-import "@fontsource/tangerine"
-import "@fontsource/raleway"
+import "@fontsource/tangerine";
+import "@fontsource/raleway";
 import Menu from './Menu';
-import Subscribe from './Subscribe';
 import Footer from './Footer';
 import styled from 'styled-components';
 import Container from 'react-bootstrap/Container'
@@ -20,25 +19,26 @@ const GlobalStyle = createGlobalStyle`
     background-color: #202122;
     color: #FCFDFE;
     margin: 0;
-
     font-family: 'Raleway', sans-serif;
 
     h1, h2, h3, h4, h5, h6 {
       font-family: 'Tangerine', serif;
     }
-
+${'' /* 
     .bg {
       top: 0;
       position: absolute;
       z-index: -5;
-    }
+    } */}
   }
 `
 const Logo = styled.h1`
   font-size: 96px;
   margin: 2rem 0 2rem 2rem;
   text-shadow: 7px 7px 4px #202020;
-  text-align: center; 
+  text-align: center;
+  min-height: 250px;
+  max-height: 300px; 
 
   a {
     text-decoration: none;
@@ -57,19 +57,14 @@ const Logo = styled.h1`
 export default function Layout({ children }) {
   return (
     <>
-      <StaticImage className="bg" src="../images/HeaderwotextDark.jpg" alt="" />
+      {/* <StaticImage className="bg" src="../images/HeaderwotextDark.jpg" alt="" /> */}
       <GlobalStyle />
       <Logo><Link to='/'>Renee Ross Books</Link>
         <div role="doc-subtitle">Gothic Romance the Way You Remember...</div>
       </Logo>
       <Menu/>
-      <Container>
-        <Row>
-          <Col sm={8}>{children}</Col>
-          <Col sm={4}>
-            <Subscribe/>
-          </Col>
-        </Row>
+      <Container fluid>
+        {children}
       </Container>
       <Footer/>
     </>
