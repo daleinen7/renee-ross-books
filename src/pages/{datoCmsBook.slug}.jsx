fcd8b7506c4436b5768f15764a5ef76f903ebf9c
Stephanie <Rinehart>
2021-07-05 20:34:59 -0400

    footer styling and add web dev attrib

--- src/pages/{datoCmsBook.slug}.jsx
+++ src/pages/{datoCmsBook.slug}.jsx
@@ -20,7 +20,9 @@ export default function Book({data}) {
           <GatsbyImage
             image={image}
             alt={data.datoCmsBook.title}
-            style={{ boxShadow: "1px 1px 1px 1px gray, 10px 10px 0 1px white", margin: "20px 50px", float: "left"}}/>
+            style={{ boxShadow: "1px 1px 1px 1px gray, 10px 10px 0 1px white", margin: "20px 50px", float: "left" }}
+            className="zoom"
+            />
           <StructuredText data={data.datoCmsBook.description} />
         </Col>
       </Row>
