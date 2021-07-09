d2ccc34b31954368661d85bc01c050fd9b8b402c
daleinen7 <daleinen@gmail.com>
2021-07-09 17:07:50 -0500

    sticky footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import {
 export default function Footer() {
   return(
     <footer>
-      <Row expand="lg" className="p-4 mb-4 navi bg-secondary footer">
+      <Row expand="lg" className="p-4 navi bg-secondary footer">
         <p>Copyright &copy; {new Date().getFullYear()} Renee Ross All Rights Reserved</p><br />
         <p className="subscript">Website Magic performed by <Link to="https://www.dougleinen.com/">Doug the Magnificent</Link> & <Link to="https://stephrinehart.com">The Spectacular Stephanie</Link></p>
       </Row>
